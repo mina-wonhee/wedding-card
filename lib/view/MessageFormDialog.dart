f0259d99ec34e45256be586ea81c1b263a8f93e6
Wonhee <mina.wonhee@gmail.com>
2021-09-08 22:41:17 +0900

    fix MessageFormDialog

--- lib/view/MessageFormDialog.dart
+++ lib/view/MessageFormDialog.dart
@@ -54,109 +54,114 @@ class _MessageFormDialogState extends State<MessageFormDialog> {
       ),
       elevation: 0,
       backgroundColor: Colors.transparent,
-      child: Container(
-        padding: EdgeInsets.only(left: 20, top: 20 + 20, right: 20, bottom: 20),
-        decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
-            ]),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: Text(
-                  "축하 메세지를 남겨주세요",
-                  style: TextStyle(
-                    fontSize: 27,
-                    color: Color.fromARGB(255, 84, 84, 84),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              TextFormField(
-                obscureText: false,
-                controller: nameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '이름',
-                ),
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return '이름을 입력해 주세요.';
-                  } else
-                    return null;
-                },
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                obscureText: true,
-                controller: pwController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '비밀번호',
-                ),
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return '비밀번호를 입력해 주세요.';
-                  } else
-                    return null;
-                },
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                obscureText: false,
-                controller: msgController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '축하 메세지',
-                ),
-                maxLines: 5,
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return '축하 메세지를 입력해 주세요.';
-                  } else
-                    return null;
-                },
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: writeCeleMessage,
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(left: 20, top: 20 + 20, right: 20, bottom: 20),
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
+              ]),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
                   child: Text(
-                    '축하 메세지 작성',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.only(left: 50, right: 50),
-                    primary: Colors.brown, // background
+                    "축하 메세지를 남겨주세요",
+                    style: TextStyle(
+                      fontSize: 27,
+                      color: Color.fromARGB(255, 84, 84, 84),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 25,
+                ),
+                TextFormField(
+                  obscureText: false,
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '이름',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return '이름을 입력해 주세요.';
+                    } else
+                      return null;
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  controller: pwController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '비밀번호',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return '비밀번호를 입력해 주세요.';
+                    } else
+                      return null;
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: false,
+                  controller: msgController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '축하 메세지',
+                  ),
+                  textInputAction: TextInputAction.done,
+                  maxLines: 5,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return '축하 메세지를 입력해 주세요.';
+                    } else
+                      return null;
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: writeCeleMessage,
+                    child: Text(
+                      '축하 메세지 작성',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.only(left: 50, right: 50),
+                      primary: Colors.brown, // background
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
