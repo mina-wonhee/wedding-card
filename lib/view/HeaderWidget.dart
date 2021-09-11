d1e0a1d9e0c38e15732d4f69dde951c1f7e5c5e6
Wonhee <mina.wonhee@gmail.com>
2021-09-11 18:46:07 +0900

    replace stful to header

--- lib/view/HeaderWidget.dart
+++ lib/view/HeaderWidget.dart
@@ -1,8 +1,13 @@
-
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-class HeaderWidget extends StatelessWidget {
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  _HeaderWidgetState createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
